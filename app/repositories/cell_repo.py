03bbbbc6fb5a8b3a1c49c7f4c3a051ef8aaf23c9
danilov-dev <dan.forth.ant@gmail.com>
2026-04-09 14:49:12 +0300

    Added cell repository

---
+++ app/repositories/cell_repo.py
@@ -0,0 +1,7 @@
+from app.repositories.base import BaseRepository
+from app.models.cell import Cell
+
+
+class CellRepository(BaseRepository[Cell]):
+    """Репозиторий для работы с сущностью Cell."""
+    pass
